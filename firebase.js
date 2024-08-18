2c15c0ba7ac0113464ed6a686f8ba2064037564e
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-08-18 12:02:33 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,18 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBJ2VyAmj5kLRXbwEu5rQk1DTvxsJweLzw",
-    authDomain: "bloging-web-34549.firebaseapp.com",
-    projectId: "bloging-web-34549",
-    storageBucket: "bloging-web-34549.appspot.com",
-    messagingSenderId: "618233084313",
-    appId: "1:618233084313:web:d55fe03a4d67d53de83b08",
-    measurementId: "G-SML9FX3F85"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyB73Kv1cQ3pTV19I5nqReVGtxX0FprbtsQ",
+  authDomain: "login-form-e13ad.firebaseapp.com",
+  projectId: "login-form-e13ad",
+  storageBucket: "login-form-e13ad.appspot.com",
+  messagingSenderId: "410563988751",
+  appId: "1:410563988751:web:ad59726ff38a8742db427a",
+  measurementId: "G-NG4MQ2N0G4"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-//   const analytics = getAnalytics(app);
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 
-
-export {app}
