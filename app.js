dc6e5e7bd8776b53ca133ca96964d411de63717d
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-08-18 18:51:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-import {app} from './firebase.js';
+// import {app} from './firebase.js';
 
+let sidebtn = document.getElementById('sidebtn');
+let sidebar = document.querySelector('.sidebar');
+let isOpen = false;
 
+function sideBar() {
+  isOpen = !isOpen;
+  if (isOpen) {
+    sidebar.style.display = "none";
+    // sidebar.classList.remove("d-flex")
+    // console.log("hello");
+    
+} 
+else {
+    sidebar.style.display = "flex";
 
+    // sidebar.classList.add("d-flex")
+  }
+ }
 
-
+sidebtn.addEventListener('click' , sideBar)
 
 
 
