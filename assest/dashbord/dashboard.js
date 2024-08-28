3741637ba177dbffe95b0d714d0fa9f2deaa5cc9
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-08-28 18:27:46 +0500

    Update dashboard.js

--- assest/dashbord/dashboard.js
+++ assest/dashbord/dashboard.js
@@ -6,6 +6,8 @@ const logOutBtn = document.querySelector(".logout");
 const sidebar = document.querySelector('.sidebar');
 let isOpen = false;
 
+
+// side bar function 
 function sideBar() {
   isOpen = !isOpen;
   if (isOpen) {
@@ -16,6 +18,7 @@ else {
   }
  }
 
+//  logOut function 
 const logOut = ()=>{
     signOut(auth).then(() => {
         Toastify({
@@ -31,14 +34,12 @@ const logOut = ()=>{
     
 }
 
-
+// state change function 
 onAuthStateChanged(auth, (user) => {
     if (!user) {
         window.location.href = '../../index.html'
     }
   });
-
-
 
 logOutBtn.addEventListener('click', logOut);
 moblogout.addEventListener('click', logOut);
