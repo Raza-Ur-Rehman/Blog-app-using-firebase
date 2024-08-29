37184ab7425c046646a2b3abbadf4698fa7d8008
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-08-29 22:02:53 +0500

    Update dashboard.js

--- assest/dashbord/dashboard.js
+++ assest/dashbord/dashboard.js
@@ -1,69 +1,87 @@
-import {auth,  signOut, onAuthStateChanged } from '../../firebase.js'
+import { auth, signOut, onAuthStateChanged , db, collection, addDoc } from "../../firebase.js";
 
-const sidebtn = document.getElementById('sidebtn');
-const moblogout = document.getElementById('moblogout');
+const sidebtn = document.getElementById("sidebtn");
+const moblogout = document.getElementById("moblogout");
 const logOutBtn = document.querySelector(".logout");
-const sidebar = document.querySelector('.sidebar');
+const sidebar = document.querySelector(".sidebar");
 let isOpen = false;
-// side bar function 
+// side bar function
 function sideBar() {
   isOpen = !isOpen;
   if (isOpen) {
     sidebar.style.display = "none";
-} 
-else {
+  } else {
     sidebar.style.display = "flex";
   }
- }
+}
 
-//  logOut function 
-const logOut = ()=>{
-    signOut(auth).then(() => {
-        Toastify({
-            text: "LogOut Successfully",
-            duration: 3000    
-            }).showToast();
-    }).catch((error) => {
-        Toastify({      
-            text: error,
-            duration: 3000 
-            }).showToast();
+//  logOut function
+const logOut = () => {
+  signOut(auth)
+    .then(() => {
+      Toastify({
+        text: "LogOut Successfully",
+        duration: 3000,
+      }).showToast();
+    })
+    .catch((error) => {
+      Toastify({
+        text: error,
+        duration: 3000,
+      }).showToast();
     });
-    
-}
+};
 
-// state change function 
+// state change function
 onAuthStateChanged(auth, (user) => {
-    if (!user) {
-        window.location.href = '../../index.html'
-    }
-  });
+  if (!user) {
+    window.location.href = "../../index.html";
+  }
+});
 
-logOutBtn.addEventListener('click', logOut);
-moblogout.addEventListener('click', logOut);
-sidebtn.addEventListener('click' , sideBar);
+logOutBtn.addEventListener("click", logOut);
+moblogout.addEventListener("click", logOut);
+sidebtn.addEventListener("click", sideBar);
 
-// blog page
+// blog page function
 
-// const blogTitle = document.getElementById('blog-title');
-// const blogContent = document.getElementById('blog-content');
-// const blogAuthor = document.getElementById('blog-author');
-// const blogDate = document.getElementById('blog-date');
-// const blogImage = document.getElementById('blog-image');
-let formFeild = document.querySelectorAll(".blogmodal form input");
-const [inputTitle, inputContent, inputAuthor,inputDate, inputImage,inputCategory] = formFeild;
-const addBlogBtn = document.getElementById('add-blog-btn');
+let formFeild = document.querySelectorAll(
+  ".blogmodal form input, textarea, select"
+);
+const [inputTitle, inputAuthor, inputDate, inputImage] = formFeild;
+const addBlogBtn = document.getElementById("add-blog-btn");
 
-const createblog = () => {
-    event.preventDefault();
+const createblog = async() => {
+  event.preventDefault();
+  if (
+    inputTitle.value.trim() !== "" &&
+    inputAuthor.value.trim() !== "" &&
+    inputDate.value.trim() !== "" &&
+    inputImage.value.trim() !== "" &&
+    inputCategory.value.trim() !== "" &&
+    inputContent.value.trim() !== ""
+  ) {
+    try {
+        const docRef = await addDoc(collection(db, "Blog"), {
+          Title: inputTitle.value,
+          Author: inputAuthor.value,
+          Date: inputDate.value,
+          Category: inputCategory.value,
+          Content : inputContent.value
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+
     
+  } else {
+    Toastify({
+      text: "All fields are required",
+      duration: 3000,
+    }).showToast();
+    return;
+  }
+};
 
-    
-}
-
-addBlogBtn.addEventListener('click', createblog);
-
-
-
-
-
+addBlogBtn.addEventListener("click", createblog);
