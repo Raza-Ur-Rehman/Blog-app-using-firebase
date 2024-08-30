dd204f8d89c2a0c149db81cde163fa62d0e858d0
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-08-31 01:12:46 +0500

    Update blog.js

--- assest/auth/blog.js
+++ assest/auth/blog.js
@@ -12,22 +12,30 @@ const showBlogs = async () => {
           blogCard.innerHTML += `
               <div class="col-lg-3 mycard">
                   <div class="card">
-                      <div class="card-img position-relative">
-                          <a href="#" class="btn text-bg-light position-absolute" id="blog-category">${Category}</a>
-                          <img src="../image/img1.png" alt="" class="object-fit-cover" id="blog-image">
-                      </div>
-                      <div class="card-body">
-                          <h5 class="card-title" id="blog-title">
+                    <div class="card-img position-relative">
+                        <a href="#" class="btn text-bg-light position-absolute" id="blog-category">${Category}</a>
+                        <img src="../image/img1.png" alt="" class="object-fit-cover" id="blog-image">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title" id="blog-title">
                             ${Title}
-                          </h5>
-                          <p class="card-text" id="blog-content">
+                        </h5>
+                        <p class="card-text" id="blog-content">
                             ${Content}
-                          </p> 
-                      </div>
-                      <div class="card-body d-flex justify-content-between">
-                          <p class="card-text ">Author : <span id="blog-author">${Author} </span> </p>
-                          <p class="card-text ">Date : <span id="blog-date"> ${Date}</span></p>
-                      </div>
+                        </p> 
+                    </div>
+                    <div class="card-body d-flex justify-content-between">
+                        <p class="card-text ">Author : <span id="blog-author">${Author} </span> </p>
+                        <p class="card-text ">Date : <span id="blog-date"> ${Date}</span></p>
+                    </div>
+                    <div class="mb-3 d-flex justify-content-around">
+                        <button class="btn fs-5 fw-semibold">
+                            <i class="fas fa-edit "></i> Edit
+                        </button>
+                        <button class="btn fs-5 fw-semibold">
+                         <i class="fas fa-trash-alt"></i> Delete
+                        </button>
+                    </div>
                   </div>
               </div>`;
         });
